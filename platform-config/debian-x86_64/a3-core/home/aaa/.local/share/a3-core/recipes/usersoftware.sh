9c80bb3221086fb55ee3ef928f22862b90e260de
rafjagger <p5hcu1@systemli.org>
2026-02-22 23:31:10 +0100

    cleanup

--- platform-config/debian-x86_64/a3-core/home/aaa/.local/share/a3-core/recipes/usersoftware.sh
+++ platform-config/debian-x86_64/a3-core/home/aaa/.local/share/a3-core/recipes/usersoftware.sh
@@ -12,7 +12,8 @@ echo "Installing TAL Filter vst..."
 wget https://tal-software.com/downloads/plugins/TAL-Filter-2_64_linux.zip
 unzip TAL-Filter-2_64_linux.zip
 rm TAL-Filter-2_64_linux.zip
-mv TAL-Filter-2 ~/.local/vst/
+mv TAL-Filter-2/TAL-Filter-2.vst3 ~/.local/vst/
+rm -rf TAL-Filter-2
 echo "done. /home/aaa/.local/vst ..."
 
 echo "Installing Airwindows vst..."
@@ -20,5 +21,6 @@ wget https://github.com/baconpaul/airwin2rack/releases/download/DAWPlugin/Airwin
 unzip AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
 rm AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
 mv awcons-products/Airwindows\ Consolidated.vst3 /home/aaa/.local/vst/
+rm -rf awcons-products
 echo "done. /home/aaa/.local/vst ..."
 
