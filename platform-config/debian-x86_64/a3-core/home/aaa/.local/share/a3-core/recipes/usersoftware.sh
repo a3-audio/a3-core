582052b5edb17d9e9b9d75f4a286da4d01430fc9
rafjagger <p5hcu1@systemli.org>
2026-02-22 23:07:27 +0100

    vst plugins

---
+++ platform-config/debian-x86_64/a3-core/home/aaa/.local/share/a3-core/recipes/usersoftware.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Installing reaper..."
+wget https://www.reaper.fm/files/7.x/reaper712_linux_x86_64.tar.xz
+tar -xf reaper712_linux_x86_64.tar.xz
+cd reaper_linux_x86_64
+./install-reaper.sh --install /home/aaa/.local/bin/ --usr-local-bin-symlink --quiet
+cd ..
+rm -rf reaper_linux_x86_64
+rm reaper712_linux_x86_64.tar.xz
+
+echo "Installing TAL Filter vst..."
+wget https://tal-software.com/downloads/plugins/TAL-Filter-2_64_linux.zip
+unzip TAL-Filter-2_64_linux.zip
+rm TAL-Filter-2_64_linux.zip
+mv TAL-Filter-2 ~/.local/vst/
+echo "done. /home/aaa/.local/vst ..."
+
+echo "Installing Airwindows vst..."
+wget https://github.com/baconpaul/airwin2rack/releases/download/DAWPlugin/AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
+unzip AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
+rm AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
+mv awcons-products/Airwindows\ Consolidated.vst3 ~/.local/vst/
+echo "done. /home/aaa/.local/vst ..."
+
