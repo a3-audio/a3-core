35b28d73dd05f10500bfc08777cdd8b92793dcbf
rafjagger <p5hcu1@systemli.org>
2026-02-22 20:47:45 +0100

    reaper setup

--- platform-config/debian-x86_64/a3-core/home/aaa/.local/share/a3-core/recipes/reaper_oneshot_install.sh
+++ platform-config/debian-x86_64/a3-core/home/aaa/.local/share/a3-core/recipes/reaper_oneshot_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Installing reaper..."
 wget https://www.reaper.fm/files/7.x/reaper712_linux_x86_64.tar.xz
-tar xf reaper712_linux_x86_64.tar.xz
+tar -xf reaper712_linux_x86_64.tar.xz
 cd reaper_linux_x86_64
-sudo ./install-reaper.sh --install /opt --no-desktop --usr-local-bin-symlink
+./install-reaper.sh --install /opt --usr-local-bin-symlink --quiet
 cd ..
 rm -rf reaper_linux_x86_64
+rm reaper712_linux_x86_64.tar.xz
