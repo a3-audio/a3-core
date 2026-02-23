274f44afe58025c490e026d60109ca291adf8b50
rafjagger <p5hcu1@systemli.org>
2026-02-23 09:45:53 +0100

    config structure

--- platform-config/debian-x86_64/a3-core/home/aaa/.local/share/a3-core/recipes/reaper_install.sh
+++ platform-config/debian-x86_64/a3-core/home/aaa/.local/share/a3-core/recipes/reaper_install.sh
@@ -8,12 +8,14 @@ cd ..
 rm -rf reaper_linux_x86_64
 rm reaper712_linux_x86_64.tar.xz
 
-unzip -o /home/aaa/.local/share/a3-core/reaper/reaper_config.zip -d /home/aaa/.config/REAPER
+cp -rn /home/aaa/.local/share/a3-core/config/* /home/aaa/.config/
+unzip -o /home/aaa/.config/REAPER/reaper_config.zip -d /home/aaa/.config/REAPER/
 
 echo "Installing TAL Filter vst..."
 wget https://tal-software.com/downloads/plugins/TAL-Filter-2_64_linux.zip
 unzip -o TAL-Filter-2_64_linux.zip
 rm TAL-Filter-2_64_linux.zip
+rm -rf /home/aaa/.local/vst/TAL-Filter-2.vst3
 mv -f TAL-Filter-2/TAL-Filter-2.vst3 /home/aaa/.local/vst
 rm -rf TAL-Filter-2
 echo "done: /home/aaa/.local/vst/TAL-Filter-2.vst3"
@@ -22,6 +24,7 @@ echo "Installing Airwindows vst..."
 wget https://github.com/baconpaul/airwin2rack/releases/download/DAWPlugin/AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
 unzip -o AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
 rm AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
+rm -rf /home/aaa/.local/vst/Airwindows\ Consolidated.vst3
 mv -f awcons-products/Airwindows\ Consolidated.vst3 /home/aaa/.local/vst
 rm -rf awcons-products
 echo "done: /home/aaa/.local/vst/AirwindowsConsolidated.vst3"
