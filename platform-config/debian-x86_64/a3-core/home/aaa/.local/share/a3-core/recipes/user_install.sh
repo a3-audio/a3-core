5cc348393acfbc0668fc45a94186754dfa5d428a
rafjagger <p5hcu1@systemli.org>
2026-02-26 21:31:22 +0100

    testing packages / reaper config

--- platform-config/debian-x86_64/a3-core/home/aaa/.local/share/a3-core/recipes/user_install.sh
+++ platform-config/debian-x86_64/a3-core/home/aaa/.local/share/a3-core/recipes/user_install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#### Install Reaper
+
 echo "Installing reaper..."
 wget https://www.reaper.fm/files/7.x/reaper712_linux_x86_64.tar.xz
 tar -xf reaper712_linux_x86_64.tar.xz
@@ -7,18 +10,21 @@ cd reaper_linux_x86_64
 cd ..
 rm -rf reaper_linux_x86_64
 rm reaper712_linux_x86_64.tar.xz
-
 unzip -o /home/aaa/.config/REAPER/reaper_config.zip -d /home/aaa/.config/REAPER/
 rm /home/aaa/.config/REAPER/reaper_config.zip
+
+#### Install TAL Filter
 
 echo "Installing TAL Filter vst..."
 wget https://tal-software.com/downloads/plugins/TAL-Filter-2_64_linux.zip
 unzip -o TAL-Filter-2_64_linux.zip
 rm TAL-Filter-2_64_linux.zip
 rm -rf /home/aaa/.local/vst/TAL-Filter-2.vst3
-cp -rf TAL-Filter-2/TAL-Filter-2.vst3 /home/aaa/.local/vst
+cp -rf TAL-Filter-2/TAL-Filter-2.vst3 /home/aaa/.local/vst/
 rm -rf TAL-Filter-2
 echo "done: /home/aaa/.local/vst/TAL-Filter-2.vst3"
+
+#### Install Airwindows
 
 echo "Installing Airwindows vst..."
 wget https://github.com/baconpaul/airwin2rack/releases/download/DAWPlugin/AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
@@ -27,10 +33,12 @@ rm AirwindowsConsolidated-2026-02-22-b0ec35c-Linux.zip
 rm -rf /home/aaa/.local/vst/Airwindows\ Consolidated.clap
 mkdir -p /home/aaa/.local/clap
 mv -f awcons-products/Airwindows\ Consolidated.clap /home/aaa/.local/clap/airwindows.clap
-
 rm -rf awcons-products
 echo "done: /home/aaa/.local/clap/AirwindowsConsolidated.clap"
+
+#### Install Beat Analyzer
 
 echo "Installing Beat Analyzer..."
 cd ~ && git clone --recurse-submodules https://github.com/rafjagger/beat-analyzer.git
 cd beat-analyzer && ./build.sh && cp .env.example build/.env
+
